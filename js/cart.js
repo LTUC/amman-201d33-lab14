74ed80c933faa0ae9d7d4e34fa55490abd46558f
amani-alnobani <amanialnobani@gmail.com>
2021-07-28 23:00:23 +0300

    editing

--- js/cart.js
+++ js/cart.js
@@ -70,9 +70,11 @@ function showCart() {
 function removeItemFromCart(event) {
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+
+  
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
-
+  renderCart();
 }
 
 // This will initialize the page and draw the cart on screen
