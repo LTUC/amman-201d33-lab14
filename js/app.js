920ac2fa279ce78acdcced0c41e7a1781da97efe
Haneen Haashlamoun <haneen.hashlamoun@gmail.com>
2021-07-28 23:50:34 +0300

    HaneenHashlamoun Lab14

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ Cart.prototype.addItem = function (product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
    let newItem  = new CartItem(product,quantity);
    this.items.push(newItem);
-
 };
 
 Cart.prototype.saveToLocalStorage = function () {
@@ -20,6 +19,7 @@ Cart.prototype.saveToLocalStorage = function () {
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
+  // localStorage.removeItem('cart', JSON.stringify(this.item));  
   // Note: You will have to decide what kind of parameter to pass in here!
 };
 
