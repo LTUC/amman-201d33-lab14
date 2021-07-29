d19c162301e29d953e52725d0cc5674573c26a5b
Amer-1987 <amer_1987_99@yahoo.com>
2021-07-29 03:27:24 +0300

    lab14

--- js/cart.js
+++ js/cart.js
@@ -19,17 +19,46 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  document.getElementById("tbody").deleteRow(1) // taken from w3schools
+
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
+  let tableBody = document.getElementById('cart');
 
   // TODO: Iterate over the items in the cart
+  for (let i = 0; i < array.length; i++) {
+    
+
+
+
+  }
   // TODO: Create a TR
+
+  let tr = document.createElement ('tr');
+
   // TODO: Create a TD for the delete link, quantity,  and the item
+  tdRemove = document.createElement('td');
+  tr.appendChild(tdRemove);
+
+  let tdQuantity = document.createElement('td');
+  tr.appendChild(tdQuantity);
+
+  let tdItem = document.createElement('td');
+  tr.appendChild(tdItem);
+
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
+
+  bodyCont.appendChild(tr);
+
+
+
+
+
 
 }
 
@@ -37,8 +66,9 @@ function removeItemFromCart(event) {
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
+  
   // TODO: Re-draw the cart table
-
+showCart();
 }
 
 // This will initialize the page and draw the cart on screen
