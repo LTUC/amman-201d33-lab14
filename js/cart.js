81747b45b3bc316949c9783f5de4dc6131ba8b7c
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-07-29 03:28:31 +0300

    lab14

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,7 @@
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
 const table = document.getElementById('cart');
 table.addEventListener('click', removeItemFromCart);
+
 let cart;
 
 function loadCart() {
@@ -19,12 +20,41 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() 
+{
+  let trClear=document.createElement('tr')
+  table.appendChild(trClear)
+  trClear.textContent=''
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
+  let tbody=document.getElementsByTagName('tbody')[0]
+  table.appendChild(tbody)
+  tbody.textContent=''
+
+  for(let i=0;i<Cart.length;i++)
+  {
+  let tr=document.createElement('tr')
+  tbody.appendChild(tr)
+
+  let td1=document.createElement('td')
+  tr.appendChild(td1)
+
+  let td2=document.createElement('td')
+  tr.appendChild(td2)
+
+  let td3=document.createElement('td')
+  tr.appendChild(td3)
+
+    td1.textContent='X'
+    td2.textContent= quantity
+    td3.textContent= items
+  
+  }
+
 
   // TODO: Iterate over the items in the cart
   // TODO: Create a TR
@@ -32,13 +62,15 @@ function showCart() {
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
+loadCart()
 
-function removeItemFromCart(event) {
-
+function removeItemFromCart(event) 
+{
+  localStorage.removeItem(this.value)
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
-
+  loadCart()
 }
 
 // This will initialize the page and draw the cart on screen
